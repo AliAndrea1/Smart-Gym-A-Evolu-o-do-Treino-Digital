cc49890a9ad39c4e7e5a1e6851095a324ce734cc
Ali Andrea <99283192+AliAndrea1@users.noreply.github.com>
2026-04-13 20:24:35 -0300

    Adicionando arquivo do arduino

---
+++ RFID.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(115200);
+  SPI.begin();
+  rfid.PCD_Init();
+  
+  Serial.println("Aproxime sua tag do leitor...");
+}
+
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  String strID = "";
+
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    strID += (rfid.uid.uidByte[i] < 0x10 ? "0" : "");
+    strID += String(rfid.uid.uidByte[i], HEX);
+    if (i < rfid.uid.size - 1) strID += ":";
+  }
+
+  strID.toUpperCase();
+
+  Serial.print("Tag detectada! UID: ");
+  Serial.println(strID);
+
+  rfid.PICC_HaltA();
+  rfid.PCD_StopCrypto1();
+}
